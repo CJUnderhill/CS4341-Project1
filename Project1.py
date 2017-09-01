a01769b601772dc756d71aaf8576aefc9c2ffd3c
Daniel Kim <daniel.jaewoo.kim@gmail.com>
2017-08-31 20:35:30 -0400

    Added General_Search, depthFirst, breadthFirst

---
+++ Project1.py
@@ -0,0 +1,70 @@
+# Daniel Kim, Spyridon Antonatos 8/31/2017
+
+# Import functions from parser.py file for parsing the graph text files
+# into networkx graph data structures
+import graphParser
+
+# networkx graph, search function -> queue
+# Takes in a networkx graph and a search function and
+# returns an error message if 'G' was not found, and
+# returns the solution if 'G' was found
+#
+# A general search method to be called by all specific search methods
+def General_Search(graph, search_method):
+    # Initialize the queue with 'S' (Assume 'S' is always in the queue)
+    queue = [['S']]
+    # Loop
+    while (True):
+        # If the queue is empty, 'G' was not found, so return failure message
+        if (len(queue) == 0):
+            print("Error: Empty queue!")
+            sys.exit(1)
+        # If the queue is not empty, set the node variable to the first node
+        # in the first path of the queue
+        node = queue[0][0]
+        # Check if a solution was found
+        if node == 'G':
+            print(queue)
+            print("goal reached!")
+            return queue[0]
+        # Expand the current node to it's neighboring nodes
+        opened_nodes = graph.neighbors(node)
+        # Reset the queue based on the search method input
+        queue = search_method(opened_nodes, queue)
+
+# Depth 1st search method
+def depthFirst(neighbors, queue):
+    print(queue)
+    # enqueue to front
+    newQueue = []
+    neighbors.sort()
+    for n in neighbors:
+        if n in queue[0]:
+            continue
+        tempQueue = []
+        tempQueue.append(n)
+        tempQueue.extend(queue[0])
+        newQueue.append(tempQueue)
+    newQueue.extend(queue[1:])
+    return newQueue
+
+# Breadth 1st search method
+def breadthFirst(neighbors, queue):
+    # enqueue to end
+    print(queue)
+    newQueue = []
+    neighbors.sort()
+    newQueue.extend(queue[1:])
+    for n in neighbors:
+        if n in queue[0]:
+            continue
+        tempQueue = []
+        tempQueue.append(n)
+        tempQueue.extend(queue[0])
+        newQueue.append(tempQueue)
+    return newQueue
+
+# Testing general search method
+graph = graphParser.build_graph('graph.txt')
+General_Search(graph[0], depthFirst)
+General_Search(graph[0], breadthFirst)
