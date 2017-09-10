d182a68be70dc35928b10c1fd1cc2f2d729e29c0
Daniel Kim <daniel.jaewoo.kim@gmail.com>
2017-09-10 13:21:11 -0400

    Added individual functions

--- Main.py
+++ Main.py
@@ -5,6 +5,40 @@ def DepthFirstSearch(graph_data):
     Q, out = General_Search(graph_data, depthFirst, {'h':0})
     return Q, out
 
+def BreadthFirstSearch(graph_data):
+    Q, out = General_Search(graph_data, breadthFirst, {'h':0})
+    return Q, out
+
+def DepthLimitedSearch(graph_data):
+    Q, out = General_Search(graph_data, depthLimited, {'h':0, "limit":2})
+    return Q, out
+
+# SPECIAL
+def IterativeDeepeningSearch(graph_data):
+    Q, out = General_Search(graph_data, iterativeDeepening, {'h':0})
+    return Q, out
+
+def UniformSearch(graph_data):
+    Q, out = General_Search(graph_data, uniformCost, {'h':0})
+    return Q, out
+
+def GreedySearch(graph_data):
+    Q, out = General_Search(graph_data, greedySearch, {'h':11})
+    return Q, out
+
+def AStarSearch(graph_data):
+    Q, out = General_Search(graph_data, aStarSearch, {'h':11})
+    return Q, out
+
+def HillClimbingSearch(graph_data):
+    Q, out = General_Search(graph_data, hillClimbing, {'h':11})
+    return Q, out
+
+def BeamSearch(graph_data):
+    Q, out = General_Search(graph_data, beam, {'h':11})
+    return Q, out
+
+
 # Print
 def printQueue(output, informed):
     print()
@@ -45,38 +79,38 @@ def main(file):
     printQueue(out, False)
 
     print("Breadth 1st search")
-    Q, out = General_Search(graph_data, breadthFirst, {'h':0})
+    Q, out = BreadthFirstSearch(graph_data)
     printQueue(out, False)
 
     print("Depth-limited search (limit = 2)")
-    Q, out = General_Search(graph_data, depthLimited, {'h':0, "limit":2})
+    Q, out = DepthLimitedSearch(graph_data)
     printQueue(out, False)
 
     print("Iterative deepening search")
-    Q, out = General_Search(graph_data, iterativeDeepening, {'h':0})
+    IterativeDeepeningSearch(graph_data)
     printQueue(out, False)
     # FIX THIS
 
+    # SORT LEXOGRAPHICALLY FOR ALL
     print("Uniform Search (Branch-and-bound)")
-    Q, out = General_Search(graph_data, uniformCost, {'h':0})
+    Q, out = UniformSearch(graph_data)
     printQueue(out, True)
-    # sort lexo
 
     print("Greedy search")
-    Q, out = General_Search(graph_data, greedySearch, {'h':11})
+    Q, out = GreedySearch(graph_data)
     printQueue(out, True)
 
     print("A*")
-    Q, out = General_Search(graph_data, aStarSearch, {'h':11})
+    Q, out = AStarSearch(graph_data)
     printQueue(out, True)
 
     print("Hill climbing search (No backtracking)")
-    Q, out = General_Search(graph_data, hillClimbing, {'h':11})
+    Q, out = HillClimbingSearch(graph_data)
     printQueue(out, True)
 
     print("Beam search (w = 2)")
-    Q, out = General_Search(graph_data, beam, {'h':11})
+    Q, out = BeamSearch(graph_data)
     printQueue(out, True)
 
 main("graph.txt")
-#main("second_graph.txt")
+main("second_graph.txt")
